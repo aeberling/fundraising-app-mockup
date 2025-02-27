0f97f6adaf0e3fc3a3c75333cc238b2a18d2462c
aeberling <adamgenweb@gmail.com>
2025-02-27 16:12:22 -0700

    Add output to config file.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   transpilePackages: ['d3-shape', 'victory-vendor', 'recharts'],
   webpack: (config) => {
     // This is needed to fix issues with d3-shape and other ESM modules
